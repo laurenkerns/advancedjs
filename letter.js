184c9894335d2f4c65ac12112674713541c1276d
laurenkerns <laurenkerns@gmail.com>
2019-02-22 11:27:56 -0500

    file creation

---
+++ letter.js
@@ -0,0 +1,31 @@
+const inquirer = require("inquirer");
+
+//Letter Constructor
+const Letter = function(value){
+    this.value = value;
+    this.guess = false;
+    //if guess is true, console log the value, else return _
+    this.showGuess = () => {
+        if(this.guess === true){
+            console.log(this.value);
+        } else {
+            console.log("_");
+        }
+    };
+//check if guess is correct against the underlying character, update boolean
+//else return false
+    this.guessCorrectly = (userInput) => {
+        if(userInput === this.value) {
+            this.guess = true;
+            return true;
+        } else{
+            return false;
+        }
+    };
+};
+
+module.exports = Letter;
+
+
+
+
